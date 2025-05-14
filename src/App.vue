19f7089faa18c21b2c1002c703a382c4b90f2085
Cruz Antonio Jimenez Tadeo <49284949+cruz-tadeo@users.noreply.github.com>
2025-05-14 10:51:10 -0500

    [add] test challenge vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,71 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+import { computed, type Ref, ref } from 'vue'
+import IconTrash from '@/components/IconTrash.vue'
+
+const tasks = ref(['Hola',"hola"]);
+
+const filterItems = computed(() => tasks.value);
+
+const search:Ref<string> = ref('');
+
+
+const addItem = () => {
+  console.log('agregando');
+};
+
+const removeItem = () => {
+  console.log('eliminando');
+};
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container mx-auto my-20">
+    <div class="my-10">
+        <input
+          v-model="search"
+          type="text"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          required
+        />
     </div>
-  </header>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-8 w-10/12">
+      <div>
+        <h1 class="text-2xl font-bold mb-4">Tareas</h1>
+        <ul class="list-disc pl-5 space-y-2">
+          <li class="bg-red-500 grid grid-cols-1 md:grid-cols-2 gap-8" v-for="(task, index) in filterItems" :key="index">
+            <p>{{ index + 1 }}.- {{ task }}</p>
+            <p class="w-10 h-10"><icon-trash class="w-10 h-10 hover:cursor-pointer" @delete="removeItem"></icon-trash></p>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <form @submit.prevent="addItem" class="space-y-4">
+          <input
+            type="text"
+            id="email"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            required
+          />
 
-  <main>
-    <TheWelcome />
-  </main>
+          <button
+            type="submit"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none
+                 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto
+                 px-5 py-2.5"
+          >
+            Add
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
