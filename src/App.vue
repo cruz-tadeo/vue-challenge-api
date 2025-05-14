2787e9edbc33f04cc3d0b1df9a72104234d50c47
Steven Universe <22393225@utcancun.edu.mx>
2025-05-14 13:11:29 -0500

    [add] resolve this

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,26 @@
 import { computed, type Ref, ref } from 'vue'
 import IconTrash from '@/components/IconTrash.vue'
 
-const tasks = ref(['Hola',"hola"]);
+const tasks:Ref<string[]> = ref(['Hola',"hola"]);
 
-const filterItems = computed(() => tasks.value);
-
+const filterItems = computed(() => tasks.value.filter((task) => task.toLowerCase().includes(search.value.toLowerCase())));
+//words.filter((word) => word.length > 6)
 const search:Ref<string> = ref('');
 
+const task :Ref<string>= ref('');
 
-const addItem = () => {
-  console.log('agregando');
+const addItem = (text:string) => {
+  tasks.value.unshift(text);
+  task.value = ''
 };
 
-const removeItem = () => {
-  console.log('eliminando');
+
+
+
+const removeItem = (position:number) => {
+  tasks.value = tasks.value.filter((task,index) => index != position)
+
+  console.log('eliminando', position);
 };
 
 </script>
@@ -37,18 +44,19 @@ const removeItem = () => {
         <ul class="list-disc pl-5 space-y-2">
           <li class="bg-red-500 grid grid-cols-1 md:grid-cols-2 gap-8" v-for="(task, index) in filterItems" :key="index">
             <p>{{ index + 1 }}.- {{ task }}</p>
-            <p class="w-10 h-10"><icon-trash class="w-10 h-10 hover:cursor-pointer" @delete="removeItem"></icon-trash></p>
+            <p class="w-10 h-10"><icon-trash class="w-10 h-10 hover:cursor-pointer" @delete="removeItem(index)"></icon-trash></p>
           </li>
         </ul>
       </div>
       <div>
-        <form @submit.prevent="addItem" class="space-y-4">
+        <form @submit.prevent="addItem(task)" class="space-y-4">
           <input
             type="text"
-            id="email"
+
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
                  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             required
+            v-model="task"
           />
 
           <button
@@ -56,6 +64,7 @@ const removeItem = () => {
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none
                  focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto
                  px-5 py-2.5"
+
           >
             Add
           </button>
