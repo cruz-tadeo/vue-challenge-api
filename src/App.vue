ab342cfc4fa6994647e15233900d4fb5bb62f060
naadiaramirezx <22394170@utcancun.edu.mx>
2025-05-15 11:31:50 -0500

    Boton editar y edliminar agregados

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <script setup lang="ts">
-
 import { computed, type Ref, ref } from 'vue'
 import IconTrash from '@/components/IconTrash.vue'
+import IconEdit from '@/components/IconEdit.vue'
 
-const tasks:Ref<string[]> = ref(['Hola',"hola"]);
+const tasks: Ref<string[]> = ref(['Hola', 'hola'])
 
-const filterItems = computed(() => tasks.value.filter((task) => task.toLowerCase().includes(search.value.toLowerCase())));
+const filterItems = computed(() =>
+  tasks.value.filter((task) => task.toLowerCase().includes(search.value.toLowerCase())),
+)
 //words.filter((word) => word.length > 6)
-const search:Ref<string> = ref('');
+const search: Ref<string> = ref('')
 
-const task:Ref<string>= ref('');
+const task: Ref<string> = ref('')
 
 const indexTask = ref(0)
 
-const addItem = (text:string) => {
-  tasks.value.unshift(text);
+const addItem = (text: string) => {
+  tasks.value.unshift(text)
   task.value = ''
-};
+}
 
-const isEdit :Ref<boolean> = ref(false)
-const selectItem =(position:number) =>{
-  isEdit.value= true
-  task.value = tasks.value.find((element,index) => index == position) ?? '';
+const isEdit: Ref<boolean> = ref(false)
+const selectItem = (position: number) => {
+  isEdit.value = true
+  task.value = tasks.value.find((element, index) => index == position) ?? ''
   indexTask.value = position
   console.log(position)
 }
@@ -32,71 +34,76 @@ const selectItem =(position:number) =>{
 //   console.log('actualizar')
 //  }
 
-const editItem = (position:number, text:string) => {
-    tasks.value[position] = text
-    task.value = ''
-    isEdit.value= false
-  }
+const editItem = (position: number, text: string) => {
+  tasks.value[position] = text
+  task.value = ''
+  isEdit.value = false
+}
 
-const removeItem = (position:number) => {
-  tasks.value = tasks.value.filter((task,index) => index != position)
+const removeItem = (position: number) => {
+  tasks.value = tasks.value.filter((task, index) => index != position)
 
-  console.log('eliminando', position);
-};
-
+  console.log('eliminando', position)
+}
 </script>
 
 <template>
   <div class="container mx-auto my-20">
     <div class="my-10">
-        <input
-          v-model="search"
-          type="text"
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-                 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-          required
-        />
+      <input
+        v-model="search"
+        type="text"
+        class="bg-gray-50 border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        required
+      />
     </div>
     <div class="grid grid-cols-1 md:grid-cols-2 gap-8 w-10/12">
       <div>
+        <table class="table-fixed">
+
+        </table>
         <h1 class="text-2xl font-bold mb-4">Tareas</h1>
         <ul class="list-disc pl-5 space-y-2">
-          <li class="bg-red-500 grid grid-cols-1 md:grid-cols-2 gap-8" v-for="(task, index) in filterItems" :key="index">
-            <div @click="selectItem(index)" >
-            <p>{{ index + 1 }}.- {{ task }}</p>
-            <p class="w-10 h-10"><icon-trash class="w-10 h-10 hover:cursor-pointer" @delete="removeItem(index)"></icon-trash></p>
+          <li
+            class="bg-red-500 flex items-center justify-between p-4"
+            v-for="(task, index) in filterItems"
+            :key="index"
+          >
+            <div @click="selectItem(index)">
+              <p>{{ index + 1 }}.- {{ task }}</p>
+              <div class="flex space-x-4">
+                <icon-trash
+                  class="w-10 h-10 hover:cursor-pointer"
+                  @click="removeItem(index)"
+                ></icon-trash>
+                <icon-edit
+                  class="w-10 h-10 hover:cursor-pointer"
+                  @click="selectItem(index)"
+                ></icon-edit>
+              </div>
             </div>
           </li>
         </ul>
       </div>
-      </div>
-      <div>
-        <form @submit.prevent="!isEdit ? addItem(task) : editItem(indexTask, task)" class="space-y-4">
-          <input
-            type="text"
-
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-                 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            required
-            v-model="task"
-          />
-
-          <button
-            type="submit"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none
-                 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto
-                 px-5 py-2.5"
-
-          >
-            {{ isEdit ? 'Edit' : 'Add' }}
-          </button>
-
-        </form>
-      </div>
     </div>
+    <div>
+      <form @submit.prevent="!isEdit ? addItem(task) : editItem(indexTask, task)" class="space-y-4">
+        <input
+          type="text"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          required
+          v-model="task"
+        />
 
+        <button
+          type="submit"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5"
+        >
+          {{ isEdit ? 'Edit' : 'Add' }}
+        </button>
+      </form>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/IconTrash.vue
+++ src/components/IconTrash.vue
@@ -7,7 +7,11 @@ const emits = defineEmits<{
 </script>
 
 <template>
-  <svg @click="emits('delete')" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 30 30">
-    <path d="M 13 3 A 1.0001 1.0001 0 0 0 11.986328 4 L 6 4 A 1.0001 1.0001 0 1 0 6 6 L 24 6 A 1.0001 1.0001 0 1 0 24 4 L 18.013672 4 A 1.0001 1.0001 0 0 0 17 3 L 13 3 z M 6 8 L 6 24 C 6 25.105 6.895 26 8 26 L 22 26 C 23.105 26 24 25.105 24 24 L 24 8 L 6 8 z"></path>
-  </svg>
+  <svg @click="emits('delete')"  xmlns="http://www.w3.org/2000/svg"
+height="24px" viewBox="0 -960 960 960"
+width="24px" fill="#e3e3e3">
+<path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"></path>
+</svg>
 </template>
+
+
